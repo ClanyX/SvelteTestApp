701eced0d72f48503d0f86338839e37a3aa7e5cb
Filip <101202559+ClanyX@users.noreply.github.com>
2025-02-13 18:31:35 +0100

    some game with runes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import { writable, derived } from 'svelte/store';
+
+    let namell = writable('');
+    let lastname = writable('');
+    let age = writable('');
+    let day = writable('');
+
+    let renderText = derived([namell, lastname, age, day], ([$namell, $lastname, $age, $day]) => {
+        return [$namell, $lastname, $age.toString(), $day.toString()];
+    });
+</script>
+
+<form>
+    <label>
+        <input type="text" placeholder="name" bind:value={$namell}>
+    </label>
+    <label>
+        <input type="text" placeholder="lastname" bind:value={$lastname}>
+    </label>
+    <label>
+        <input type="number" placeholder="age" bind:value={$age}>
+    </label>
+    <label>
+        <input type="number" placeholder="day" bind:value={$day}>
+    </label>
+</form>
+
+<div>
+    <p>Name: {$renderText[0]}</p>
+    <p>Lastname: {$renderText[1]}</p>
+    <p>Age: {$renderText[2]}</p>
+    <p>Day: {$renderText[3]}</p>
+</div>
