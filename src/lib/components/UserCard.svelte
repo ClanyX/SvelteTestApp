7aa888cfa7456a24d0216ae81f6c28c6c9922a92
Filip <101202559+ClanyX@users.noreply.github.com>
2025-02-25 21:33:47 +0100

    add UserCard

---
+++ src/lib/components/UserCard.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let user = {
+        name: 'name',
+        lastname: 'lastname',   
+        age: 0,
+        telephone: '0000',
+    }
+</script>
+
+<div class="max-w-sm mx-auto bg-blue-700 rounded-2xl shadow-md shadow-gray-400 overflow-hidden p-6">
+    <div class="flex items-center space-x-4 text-gray-200 test-lg">
+        <h1 class="font-bold text-2xl">User</h1>
+        <div class="h-15 w-[2px] bg-gray-300"></div>
+        <h3>{user.name} {user.lastname}</h3>
+        <p>Age: {user.age}</p>
+        <p>Telephone: {user.telephone}</p>
+    </div>
+</div>
--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -1,46 +1,50 @@
 <script lang="ts">
-    import { invalidateAll } from '$app/navigation';
-    import type { User } from '$lib/server/database';
+  import { invalidateAll } from "$app/navigation";
+  import type { User } from "$lib/server/database";
+  import UserCard from "$lib/components/UserCard.svelte";
 
-    let users: User[] = $state([]);
+  let users: User[] = $state([]);
 
-    // Načtení seznamu uživatelů
-    $effect(() => {
-        loadUsers();
+  // Načtení seznamu uživatelů
+  $effect(() => {
+    loadUsers();
+  });
+
+  async function loadUsers() {
+    const res = await fetch("/api/users");
+    users = await res.json();
+  }
+
+  // Přidání nového uživatele
+  async function addUser() {
+    const name = prompt("Zadejte jméno uživatele");
+    const lastname = prompt("Zadejte příjmení uživatele");
+    const age = prompt("Zadejte vek uživatele");
+    const telephone = prompt("Zadejte telefon uživatele");
+
+    if (!name || !lastname || !age || !telephone) return;
+
+    const res = await fetch("/api/users", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name, lastname, age: Number(age), telephone }),
     });
 
-    async function loadUsers() {
-        const res = await fetch('/api/users');
-        users = await res.json();
-    };
-
-    // Přidání nového uživatele
-    async function addUser() {
-      const name = prompt('Zadejte jméno uživatele');
-      const lastname = prompt('Zadejte příjmení uživatele');
-      const age = prompt('Zadejte vek uživatele');
-      const telephone = prompt('Zadejte telefon uživatele');
-  
-      if (!name || !lastname || !age || !telephone) return;
-  
-      const res = await fetch('/api/users', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ name, lastname, age: Number(age), telephone }),
-      });
-
-      if(res.ok) {
-        users = await res.json();
-      }
+    if (res.ok) {
+      users = await res.json();
     }
-  </script>
+  }
+</script>
+
+<div class="flex flex-col items-center mt-2">
+  <h1 class="font-bold text-2xl">Seznam uživatelů</h1>
+  <button class="butonapp m-4" onclick={addUser}>Přidat uživatele</button>
   
-  <h1>Seznam uživatelů</h1>
-  <button class="butonapp" onclick={addUser}>Přidat uživatele</button>
-  
-  <ul>
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
     {#each users as user}
-      <li>{user.id}: Name: {user.name} | Lastname: {user.lastname} | Age: {user.age} | Telephone: {user.telephone}</li>
+      <div class="mb-4">
+        <UserCard {user} />
+      </div>
     {/each}
-  </ul>
-  
+  </div>
+</div>
