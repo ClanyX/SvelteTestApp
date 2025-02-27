8e33b7aae42e638362abb85151af2aaf1d8ddb6e
Filip <101202559+ClanyX@users.noreply.github.com>
2025-02-28 00:49:14 +0100

    disko card

--- src/lib/components/UserCard.svelte
+++ src/lib/components/UserCard.svelte
@@ -4,10 +4,26 @@
         lastname: 'lastname',   
         age: 0,
         telephone: '0000',
+    };
+
+    let color = setColor();
+
+    function colorCard(){
+        const interval = setInterval(() => {
+            color = setColor();
+        }, 800);
+        return () => clearInterval(interval);
+    }
+
+    colorCard();
+
+    function setColor(){
+        let color = Math.floor(Math.random()*16777215).toString(16).padStart(6, '0');
+        return `#${color.toUpperCase()}`;
     }
 </script>
 
-<div class="max-w-sm mx-auto bg-blue-700 rounded-2xl shadow-md shadow-gray-400 overflow-hidden p-6">
+<div class="card max-w-sm mx-auto rounded-2xl shadow-md shadow-gray-400 overflow-hidden p-6" style="background-color: {color};">
     <div class="flex items-center space-x-4 text-gray-200 test-lg">
         <h1 class="font-bold text-2xl">User</h1>
         <div class="h-15 w-[2px] bg-gray-300"></div>
@@ -16,3 +32,9 @@
         <p>Telephone: {user.telephone}</p>
     </div>
 </div>
+
+<style>
+    .card {
+        transition: background-color 1s ease-in-out;
+    }
+</style>
